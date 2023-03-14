4ad6cf23eface6bca5e14a1f2581167d9f6dd1fa
villaricadianne <91996994+villaricadianne@users.noreply.github.com>
2023-03-13 20:43:17 -0700

    Added UI. Needs additional styling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import { get } from 'lodash';
-import './stylesheets/styles.scss';
+import './sass/styles.scss';
 
 function App() {
-  const app = document.createElement('div');
-
   async function getLocationData(value) {
     try {
       const response = await fetch(
@@ -27,31 +25,86 @@ function App() {
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=de594cdf8e316a5e01f9bfc405afc36f`
       );
       const weatherData = await response.json();
-      // console.log(weatherData);
+      console.log(weatherData);
+      return weatherData;
     } catch (error) {
       console.log(error);
     }
   }
 
   // DOM STUFF
-
+  const app = document.createElement('main');
   const form = document.createElement('form');
   const input = document.createElement('input');
-  const button = document.createElement('input');
+  const button = document.createElement('button');
+  const weather = document.createElement('div');
+  const weatherMain = document.createElement('div');
+  const weatherIcon = document.createElement('span');
+  const weatherTemp = document.createElement('span');
+  const weatherToggle = document.createElement('button');
+  const weatherDesc = document.createElement('div');
+  const weatherCity = document.createElement('div');
+  const weatherOther = document.createElement('div');
+  const weatherPrecipitation = document.createElement('span');
+  const weatherHumidity = document.createElement('span');
+  const weatherWind = document.createElement('span');
 
   button.setAttribute('type', 'submit');
-  button.textContent = 'Submit';
-
   input.setAttribute('type', 'text');
+  weather.setAttribute('class', 'weather');
+  weatherMain.setAttribute('class', 'weather--main');
+  weatherIcon.setAttribute('class', 'weather--icon');
+  weatherTemp.setAttribute('class', 'weather--temp');
+  weatherToggle.setAttribute('class', 'weather--toggle');
+  weatherCity.setAttribute('class', 'weather--city');
+  weatherDesc.setAttribute('class', 'weather--desc');
+  weatherOther.setAttribute('class', 'weather--other');
+  weatherPrecipitation.setAttribute('class', 'weather--precipitation');
+  weatherHumidity.setAttribute('class', 'weather--humidity');
+  weatherWind.setAttribute('class', 'weather--wind');
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    // console.log(input.value);
-    getWeatherData(input.value);
-  });
+  button.textContent = 'Submit';
+  weatherIcon.innerHTML = '&#9788;';
+  weatherTemp.innerHTML = '66 &#8457';
+  weatherToggle.textContent = 'F | C';
+  weatherCity.textContent = 'Las Vegas, NV';
+  weatherDesc.textContent = 'clear sky';
+  weatherPrecipitation.textContent = 'Precipitation: 0%';
+  weatherHumidity.textContent = 'Humidity: 28%';
+  weatherWind.textContent = 'Wind: 6 mph';
 
   form.append(input, button);
-  app.append(form);
+  weatherMain.append(
+    weatherIcon,
+    weatherTemp,
+    weatherToggle,
+    weatherDesc,
+    weatherCity
+  );
+  weatherOther.append(weatherPrecipitation, weatherHumidity, weatherWind);
+  weather.append(weatherMain, weatherOther);
+  app.append(form, weather);
+
+  // trigger
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    (async () => {
+      try {
+        let weatherData = await getWeatherData(input.value);
+
+        let tempF = `${Math.round(
+          ((weatherData.main.temp - 273.15) * 9) / 5 + 32
+        )} &#8457;`;
+        let tempC = `${Math.round(weatherData.main.temp - 273.15)} &#8451;`;
+        let city = weatherData.name;
+
+        weatherTemp.innerHTML = tempF;
+        weatherCity.textContent = city;
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  });
 
   return app;
 }
